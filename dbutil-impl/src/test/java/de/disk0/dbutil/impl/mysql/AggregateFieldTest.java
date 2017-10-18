2ca6515a82f4689e8ae25e060f8255e19c7fec37
Ruben Malchow <ruben.malchow@mcon-group.com>
2017-10-18 12:20:17 +0200

    [ci-skip] release prepare 0.0.23

---
+++ dbutil-impl/src/test/java/de/disk0/dbutil/impl/mysql/AggregateFieldTest.java
@@ -0,0 +1,27 @@
+package de.disk0.dbutil.impl.mysql;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.disk0.dbutil.api.Aggregate;
+import de.disk0.dbutil.api.Select;
+import de.disk0.dbutil.api.TableReference;
+
+public class AggregateFieldTest {
+	
+	
+	@Test
+	public void testSimpleAggregateField() {
+		
+		Select s = new MysqlStatementBuilder().createSelect();
+		TableReference tr = s.fromTable("hund");
+		
+		s.addSelect(tr.field(Aggregate.AVG, "value"),"avg_value");
+		
+		Assert.assertEquals("SELECT AVG (`hund_1`.`value`) AS `avg_value` FROM `hund` `hund_1`", s.getSql());
+		
+		
+	}
+	
+
+}
