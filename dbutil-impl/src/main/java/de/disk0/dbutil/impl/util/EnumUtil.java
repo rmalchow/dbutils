07c904d5d6807f5aa86f19f0899893190267eeb2
Ruben Malchow <ruben.malchow@mcon-group.com>
2018-02-21 17:16:46 +0100

    added resolving of enums, also from arrays

--- dbutil-impl/src/main/java/de/disk0/dbutil/impl/util/EnumUtil.java
+++ dbutil-impl/src/main/java/de/disk0/dbutil/impl/util/EnumUtil.java
@@ -2,31 +2,39 @@ package de.disk0.dbutil.impl.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 public class EnumUtil {
 	
 	public static Object resolveEnums(Object in) {
+		List<Object> out = new ArrayList<>();
 		if(in == null) {
 			return in;
 		} else if(in.getClass().isEnum()) {
 			return ((Enum)in).name();
 		} else if(in instanceof Collection) {
-			
-			if(((Collection)in).size()==0) {
-				return in;
-			} else if(!((((Collection)in).iterator()) instanceof Enum)) {
-				return in;
+			for(Object o : ((Collection)in)) {
+				out.add(o);
 			}
+		} else if (in.getClass().isArray()) {
+			for(Object o : ((Object[])in)) {
+				out.add(o);
+			}
+		} else {
+			return in;
+		}
+
+		if(out.size()==0) {
+			return out;
+		}
+		if(out.get(0) instanceof Enum) {
 			List<String> x = new ArrayList<>();
-			Iterator<Enum> e = (Iterator<Enum>)((Collection)in).iterator();
-			while(e.hasNext()) {
-				x.add(e.next().name());
+			for(Object o : out) {
+				x.add(((Enum)o).name());
 			}
 			return x;
 		}
-		return in;
+		return out;
 	}
 	
 
