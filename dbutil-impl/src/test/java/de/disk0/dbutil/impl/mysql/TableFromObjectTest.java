6699d6f429ea3c6e493346d9a14427a6c135f57a
Gitlab CI <>
2018-07-31 17:32:51 +0800

    [ci-skip] release prepare 0.0.37

---
+++ dbutil-impl/src/test/java/de/disk0/dbutil/impl/mysql/TableFromObjectTest.java
@@ -0,0 +1,28 @@
+package de.disk0.dbutil.impl.mysql;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.disk0.dbutil.api.Aggregate;
+import de.disk0.dbutil.api.Select;
+import de.disk0.dbutil.api.TableReference;
+import de.disk0.dbutil.impl.hund.Hund;
+
+public class TableFromObjectTest {
+	
+	
+	@Test
+	public void testTableNameFromField() {
+		
+		Select s = new MysqlStatementBuilder().createSelect();
+		TableReference tr = s.fromTable(Hund.class);
+		
+		s.addSelect(tr.field(Aggregate.AVG, "value"),"avg_value");
+		
+		Assert.assertEquals("SELECT AVG (`hund_1`.`value`) AS `avg_value` FROM `hund` `hund_1`", s.getSql());
+		
+		
+	}
+	
+
+}
