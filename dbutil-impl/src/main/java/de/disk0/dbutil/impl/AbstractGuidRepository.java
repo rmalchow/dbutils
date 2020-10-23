1a8d484d37d2b576f63c9e9a0a076feb32c12879
ruben <ruben.malchow@gmail.com>
2020-10-23 12:42:13 +0200

    no empty id

--- dbutil-impl/src/main/java/de/disk0/dbutil/impl/AbstractGuidRepository.java
+++ dbutil-impl/src/main/java/de/disk0/dbutil/impl/AbstractGuidRepository.java
@@ -1,5 +1,6 @@
 package de.disk0.dbutil.impl;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 import org.apache.commons.logging.Log;
@@ -7,6 +8,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.util.StringUtils;
 
 import de.disk0.dbutil.api.entities.BaseGuidEntity;
 import de.disk0.dbutil.api.exceptions.SqlException;
@@ -26,32 +28,43 @@ public abstract class AbstractGuidRepository<T extends BaseGuidEntity> extends A
 	}
 	
 	public T save(T t, String uuid) throws SqlException {
+		NamedParameterJdbcTemplate templ = null;
 		try {
 			beforeSave(t);
-			NamedParameterJdbcTemplate templ = new NamedParameterJdbcTemplate(getDataSource());
-			if(t.getId()==null) {
+			templ = getTemplate();
+			if(StringUtils.isEmpty(t.getId())) {
 				t.setId(uuid);
 				String sql = getInsertOneStatement();
 				
 				MapSqlParameterSource params = unmap(t);
-				
-				log.debug("saving entity: insert!");
-				log.debug("INSERT: "+sql);
-				log.debug("INSERT: "+params.getValues());
-				templ.update(sql, params);
+				if(log.isDebugEnabled()) {
+					log.debug("saving entity: insert!");
+					log.debug("INSERT: "+sql);
+					log.debug("INSERT: "+params.getValues());
+				}
+				int done = templ.update(sql, params);
+				log.debug("saving entity: "+done+" rows inserted");
 			} else {
 				String sql = getUpdateOneStatement();
 				MapSqlParameterSource params = unmap(t);
-				log.debug("saving entity: update!");
-				log.debug("UPDATE: "+sql);
-				log.debug("UPDATE: "+params.getValues());
-				templ.update(sql, params);
+				if(log.isDebugEnabled()) {
+					log.debug("saving entity: update!");
+					log.debug("UPDATE: "+sql);
+					log.debug("UPDATE: "+params.getValues());
+				}
+				int done = templ.update(sql, params);
+				log.debug("saving entity: "+done+" rows updated");
 			}
+			log.debug("saving entity: calling after save ... ");
 			afterSave(t);
+			log.debug("saving entity: calling after save ... done!");
+			log.debug("saving entity: retrieving object as saved: >>>"+t.getId()+"<<<");
 			T out = get(t.getId());
+			log.debug("saving entity: retrieving object as saved: "+(out==null?"NULL":"FOUND"));
 			return out;
 		} catch (Exception e) {
 			throw new SqlException("SQL.REPO.SAVE_FAILED",new Object[] { e.getMessage() },  e);
+		} finally {
 		}
 	}	
 	
