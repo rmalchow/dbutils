471e8ba544ca44f4c667c1d192cd933ff3e43f29
Gitlab CI <ruben.malchow@gmail.com>
2021-05-10 13:26:42 +0200

    fix date format

--- dbutil-impl/src/main/java/de/disk0/dbutil/impl/util/FormatUtil.java
+++ dbutil-impl/src/main/java/de/disk0/dbutil/impl/util/FormatUtil.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class FormatUtil {
 
-	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	public static Object resolve(Object value) {
 		if(value instanceof Boolean) {
