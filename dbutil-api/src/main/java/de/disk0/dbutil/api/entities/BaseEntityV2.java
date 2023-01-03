894626e9072498be23fbfad68cc7d452fbe45b27
Martin Dendis <martin.dendis@email.cz>
2023-01-03 14:53:01 +0100

    BaseEntity fixes

--- dbutil-api/src/main/java/de/disk0/dbutil/api/entities/BaseEntityV2.java
+++ dbutil-api/src/main/java/de/disk0/dbutil/api/entities/BaseEntityV2.java
@@ -1,22 +1,17 @@
 package de.disk0.dbutil.api.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class BaseEntityV2<T> {
+public class BaseEntityV2<T> implements BaseEntity<T> {
 
 	@Id
 	@Column(name = "id", unique = true, nullable = false)
-	private T id;
-
-	public T getId() {
-		return id;
-	}
-
-	public void setId(T id) {
-		this.id = id;
-	}
+	@Getter @Setter private T id;
 
 }
--- dbutil-api/src/main/java/de/disk0/dbutil/api/entities/BaseGuidEntityV1.java
+++ dbutil-api/src/main/java/de/disk0/dbutil/api/entities/BaseGuidEntityV1.java
@@ -1,22 +1,17 @@
 package de.disk0.dbutil.api.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class BaseGuidEntityV1 extends BaseEntityV1<String> implements BaseGuidEntity {
+public class BaseGuidEntityV1  implements BaseGuidEntity {
 
 	@Id
 	@Column(name = "id", unique = true, nullable = false)
-	private String id;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
+	@Getter @Setter private String id;
 
 }
