b4a97d2138912b77d3bbc3e09ebec98e61208bdc
rm <ruben.malchow@mcon-group.com>
2023-03-04 13:08:44 +0100

    with v2 guidrepo

---
+++ dbutil-impl/src/main/java/de/disk0/dbutil/impl/AbstractGuidRepositoryV2.java
@@ -0,0 +1,112 @@
+package de.disk0.dbutil.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.util.StringUtils;
+
+import de.disk0.dbutil.api.IdGeneratorClass;
+import de.disk0.dbutil.api.IdGenerator;
+import de.disk0.dbutil.api.entities.BaseGuidEntityV2;
+import de.disk0.dbutil.api.exceptions.SqlException;
+import de.disk0.dbutil.api.utils.IdGeneratorGuid;
+
+public abstract class AbstractGuidRepositoryV2<T extends BaseGuidEntityV2> extends AbstractRepository<T,String> implements RowMapper<T> {
+
+	private static Log log = LogFactory.getLog(AbstractGuidRepositoryV2.class);
+
+	private IdGenerator idGenerator;
+	
+	 public void setIdGenerator(IdGenerator idGenerator) {
+	        assert idGenerator != null;
+	        this.idGenerator = idGenerator;
+	 }
+	 
+	public String generateId() throws SqlException {
+		if(idGenerator!=null) return idGenerator.generateId();
+		try {
+			log.info("instantiating GUID generator ... ");
+			IdGenerator idg = null;
+			Class<? extends IdGenerator> ggc = null;
+			IdGeneratorClass gc = this.getClass().getAnnotation(IdGeneratorClass.class);
+			if(gc!=null) {
+				log.info("instantiating GUID generator ... annotation: "+gc.value());
+				ggc = gc.value();
+			}
+			if(ggc!=null) {
+				log.info("instantiating GUID generator ... instance: "+gc.value());
+				idg = ggc.newInstance();
+			}
+			if(idg == null) {
+				idg = new IdGeneratorGuid();
+			}
+			log.info("instantiating GUID generator ... result: "+idg);
+			idGenerator = idg;
+			return idGenerator.generateId();
+		} catch (Exception e) {
+			log.warn("unable to instantiate id generator: ",e);
+			throw new SqlException("unable to instantiate id generator", e);
+		}
+	}
+
+
+	public int delete(String id) throws SqlException {
+		T t = get(id);
+		return super.delete(t);
+	}
+
+	public T save(T t) throws SqlException {
+		return save(t,generateId());
+	}
+	
+	public T save(T t, String uuid) throws SqlException {
+		NamedParameterJdbcTemplate templ = null;
+		try {
+			beforeSave(t);
+			templ = getTemplate();
+			if(StringUtils.isEmpty(t.getId())) {
+				t.setId(uuid);
+				String sql = getInsertOneStatement();
+				
+				MapSqlParameterSource params = unmapInsert(t);
+				if(log.isDebugEnabled()) {
+					log.debug("saving entity: insert! ("+getClazz().getSimpleName()+")");
+					log.debug("INSERT: "+sql);
+					log.debug("INSERT: "+params.getValues());
+				}
+				try {
+					int done = templ.update(sql, params);
+					log.debug("saving entity: "+done+" rows inserted");
+				} catch (Exception e) {
+					log.warn("saving failed: "+(sql));
+					log.warn("saving failed: "+(params));
+					throw e;
+					
+				}
+			} else {
+				String sql = getUpdateOneStatement();
+				MapSqlParameterSource params = unmapUpdate(t);
+				if(log.isDebugEnabled()) {
+					log.debug("saving entity: update!");
+					log.debug("UPDATE: "+sql);
+					log.debug("UPDATE: "+params.getValues());
+				}
+				int done = templ.update(sql, params);
+				log.debug("saving entity: "+done+" rows updated");
+			}
+			log.debug("saving entity: calling after save ... ");
+			afterSave(t);
+			log.debug("saving entity: calling after save ... done!");
+			log.debug("saving entity: retrieving object as saved: >>>"+t.getId()+"<<< ("+getClazz().getSimpleName()+")");
+			T out = get(t.getId());
+			log.debug("saving entity: retrieving object as saved: "+(out==null?"NULL":"FOUND"));
+			return out;
+		} catch (Exception e) {
+			throw new SqlException("SQL.REPO.SAVE_FAILED",new Object[] { e.getMessage() },  e);
+		} finally {
+		}
+	}	
+	
+}
--- dbutil-impl/src/main/java/de/disk0/dbutil/impl/util/PersistenceApiUtils.java
+++ dbutil-impl/src/main/java/de/disk0/dbutil/impl/util/PersistenceApiUtils.java
@@ -130,6 +130,24 @@ public class PersistenceApiUtils {
 		String name;
 		String catalog;
 		String schema;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCatalog() {
+			return catalog;
+		}
+		public void setCatalog(String catalog) {
+			this.catalog = catalog;
+		}
+		public String getSchema() {
+			return schema;
+		}
+		public void setSchema(String schema) {
+			this.schema = schema;
+		}
 	}
 
 	@Value
@@ -144,6 +162,66 @@ public class PersistenceApiUtils {
 		int length;
 		int precision;
 		int scale;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public boolean isUnique() {
+			return unique;
+		}
+		public void setUnique(boolean unique) {
+			this.unique = unique;
+		}
+		public boolean isNullable() {
+			return nullable;
+		}
+		public void setNullable(boolean nullable) {
+			this.nullable = nullable;
+		}
+		public boolean isInsertable() {
+			return insertable;
+		}
+		public void setInsertable(boolean insertable) {
+			this.insertable = insertable;
+		}
+		public boolean isUpdatable() {
+			return updatable;
+		}
+		public void setUpdatable(boolean updatable) {
+			this.updatable = updatable;
+		}
+		public String getColumnDefinition() {
+			return columnDefinition;
+		}
+		public void setColumnDefinition(String columnDefinition) {
+			this.columnDefinition = columnDefinition;
+		}
+		public String getTable() {
+			return table;
+		}
+		public void setTable(String table) {
+			this.table = table;
+		}
+		public int getLength() {
+			return length;
+		}
+		public void setLength(int length) {
+			this.length = length;
+		}
+		public int getPrecision() {
+			return precision;
+		}
+		public void setPrecision(int precision) {
+			this.precision = precision;
+		}
+		public int getScale() {
+			return scale;
+		}
+		public void setScale(int scale) {
+			this.scale = scale;
+		}
 	}
 
 }
