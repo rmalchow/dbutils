9b5cc49df992b9a84b3145ddeb0edb9af36e22da
rm <ruben.malchow@mcon-group.com>
2024-01-30 15:30:31 +0100

    add error on no/too many update

--- dbutil-impl/src/main/java/de/disk0/dbutil/impl/AbstractGuidRepository.java
+++ dbutil-impl/src/main/java/de/disk0/dbutil/impl/AbstractGuidRepository.java
@@ -94,6 +94,11 @@ public abstract class AbstractGuidRepository<T extends BaseGuidEntityV1> extends
 					log.debug("UPDATE: "+params.getValues());
 				}
 				int done = templ.update(sql, params);
+				if(done < 1) {
+					throw new SqlException("expecting row update, but nothing happened", null);
+				} else if (done > 1) {
+					throw new SqlException("expecting ONE row update, but more than one row was updated", null);
+				}
 				log.debug("saving entity: "+done+" rows updated");
 			}
 			log.debug("saving entity: calling after save ... ");
